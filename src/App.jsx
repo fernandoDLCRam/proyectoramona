b4f876a074e62c5265be6fe5026f74114b0030b7
Adlai CG <42752044+eriadlai@users.noreply.github.com>
2023-03-13 12:57:45 -0700

    Update
    Update Incompleto

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import nino from './assets/nino.svg'
-import nina from './assets/nina.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import nino from "./assets/nino.svg";
+import nina from "./assets/nina.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App background-image">
-      <div className='center'>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={nino} className="logo" alt="Vite logo" />
+    <div className="background-image">
+      <div className="">
+        <div className="">
+          <a href="https://goo.gl/maps/QXRnMq3eThEkQXXV8" target="_blank">
+            <img src={nino} className="logo react" alt="React logo" />
           </a>
           <a href="https://goo.gl/maps/QXRnMq3eThEkQXXV8" target="_blank">
             <img src={nina} className="logo react" alt="React logo" />
           </a>
         </div>
-
         <h2>Ven a nuestra fiesta para revelar el sexo del bebé</h2>
 
-        <hr/>
+        <hr />
 
         <div className="card">
-          <button onClick={() => window.open("https://goo.gl/maps/QXRnMq3eThEkQXXV8", "_blank")}>
+          <button
+            onClick={() =>
+              window.open("https://goo.gl/maps/QXRnMq3eThEkQXXV8", "_blank")
+            }
+          >
             Consultar Ubicación
           </button>
-          <button onClick={() => window.open("https://api.whatsapp.com/send?phone=526644826271&text=Confirmo asistencia", "_blank")}>
+          <button
+            onClick={() =>
+              window.open(
+                "https://api.whatsapp.com/send?phone=526644826271&text=Confirmo asistencia",
+                "_blank"
+              )
+            }
+          >
             Confirmar asistencia
           </button>
         </div>
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
