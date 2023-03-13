98fe9f6c1b22a83dcaa8739a3b8919ff5a2f0bf4
Adlai CG <42752044+eriadlai@users.noreply.github.com>
2023-03-13 14:15:22 -0700

    Update
    Mobile View Fixed
    Working in Pie Chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import nino from "./assets/nino.svg";
 import nina from "./assets/nina.svg";
+import BarChart from "./components/BarChart";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const clickBoy = () => {};
+  const clickGirl = () => {};
   return (
     <div className="background-image">
-      <div className="">
+      <div className="center">
         <div className="">
           <a href="https://goo.gl/maps/QXRnMq3eThEkQXXV8" target="_blank">
             <img src={nino} className="logo react" alt="React logo" />
@@ -23,23 +24,27 @@ function App() {
         <hr />
 
         <div className="card">
-          <button
-            onClick={() =>
-              window.open("https://goo.gl/maps/QXRnMq3eThEkQXXV8", "_blank")
-            }
-          >
-            Consultar Ubicación
-          </button>
-          <button
-            onClick={() =>
-              window.open(
-                "https://api.whatsapp.com/send?phone=526644826271&text=Confirmo asistencia",
-                "_blank"
-              )
-            }
-          >
-            Confirmar asistencia
-          </button>
+          <div>
+            <button
+              onClick={() =>
+                window.open("https://goo.gl/maps/QXRnMq3eThEkQXXV8", "_blank")
+              }
+            >
+              Consultar Ubicación
+            </button>
+          </div>
+          <div>
+            <button
+              onClick={() =>
+                window.open(
+                  "https://api.whatsapp.com/send?phone=526644826271&text=Confirmo asistencia",
+                  "_blank"
+                )
+              }
+            >
+              Confirmar asistencia
+            </button>
+          </div>
         </div>
       </div>
     </div>
